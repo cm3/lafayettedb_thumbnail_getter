a15bf2c13df90b00d61a277ab464318429c8b9de
KAMEDA Akihiro <cm3ak@outlook.com>
2017-01-09 00:17:15 +0900

    implement resume function; fix bugs

--- get_thumbnail.py
+++ get_thumbnail.py
@@ -4,20 +4,22 @@ import os.path
 import pandas as pd
 from pyquery import PyQuery as pq
 import urllib.parse
-from requests import get
+from requests import get, exceptions
 import sys
 import time
+import math
 
 __doc__ = """{f}
 
-Based on input csv file, download image thumbnails and add a filename column to the csv file.
-This script is not for general purpose. For crawling from digital.lafayette.edu.
-output csv filename is automatically generated as <original>_with_thumbnails.csv .
-output thumbnail filename is automatically generated based on DC.identifier.
-output directory is automatically generated based on input csv file.
+- Based on input csv file, download image thumbnails and add a filename column to the csv file.
+- This script is not for general purpose but for crawling from digital.lafayette.edu.
+- Output thumbnail filename is automatically generated based on DC.identifier.
+- Output directory is automatically generated based on input csv file.
+- If there are values in thumbnail columns already, skip those rows.
+This function enable users to continue download thumbnails when the connection is aborted.
 
 Usage:
-    {f} <input_csv>
+    {f} <csv>
     {f} -h | --help
 
 Options:
@@ -30,32 +32,41 @@ def download(url, file_name, dirpath):
         response = get(url)
         file.write(response.content)
 
-def main(input_csv):
-    df = pd.read_csv(input_csv, header=0, encoding="utf-8")
-    filename_head, ext = os.path.splitext(input_csv)
-    outputcsv_path = filename_head+"_with_thumbnails.csv"
-    outputdir_path = filename_head+"_thumbnails"
-    if not os.path.exists(outputdir_path):
+def main(csv):
+    df = pd.read_csv(csv, header=0, encoding="utf-8", dtype={"thumbnail":"str"})
+    if not "thumbnail" in list(df.columns.values): # create thumbnail column if not there
+        df["thumbnail"] = [""]*len(df)
+        # df should be reconstructed for correct pd.isnull(v["thumbnail"]) distinction
+        df.to_csv(csv, encoding="utf-8", index=False)
+        df = pd.read_csv(csv, header=0, encoding="utf-8", dtype={"thumbnail":"str"})
+    outputdir_path, ext = os.path.splitext(csv)
+    if not os.path.exists(outputdir_path): # create directory if not there
         os.mkdir(outputdir_path)
-
-    filename_list = [""]*len(df) # initialize with the length of df
-    for i, v in df.ix[:,'object_url'].iteritems():
-        print(str(i)+": "+str(v))
-        d = pq(url=v)
-        # e.g.:
-        # <meta property="og:image" content="https://digital.lafayette.edu/islandora/object/islandora:18709/datastream/JPG/view" />
-        # https://digital.lafayette.edu/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=http%3A%2F%2Fdigital.lafayette.edu%2Fislandora%2Fobject%2Fislandora%3A33760%2Fdatastream%2FJP2%2Fview&svc_id=info%3Alanl-repo%2Fsvc%2FgetRegion&svc_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajpeg2000&svc.level=1
-        url = "https://digital.lafayette.edu/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id="+urllib.parse.quote(d("meta[property='og:image']").attr('content'), safe='').replace("https","http").replace("JPG","JP2")+"&svc_id=info%3Alanl-repo%2Fsvc%2FgetRegion&svc_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajpeg2000&svc.level=1"
-        # <meta name="DC.identifier" content="islandora:18707" />
-        filename = d("meta[name='DC.identifier']").attr('content').replace(":","")+".jpg"
-        print(url)
-        print(filename)
-        download(url,filename,outputdir_path)
-        filename_list[i] = filename
-        time.sleep(3)
-    df["thumbnail"] = filename_list
-    df.to_csv(outputcsv_path, encoding="utf-8")
+    for i, v in df.iterrows():
+        if pd.isnull(v["thumbnail"]):
+            print(str(i))
+            try:
+                d = pq(url=v["object_url"])
+            except:
+                df.to_csv(csv, encoding="utf-8", index=False)
+                raise ConnectionError("error on : "+v["object_url"]+" (html)")
+            # e.g.:
+            # <meta property="og:image" content="https://digital.lafayette.edu/islandora/object/islandora:18709/datastream/JPG/view" />
+            # https://digital.lafayette.edu/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=http%3A%2F%2Fdigital.lafayette.edu%2Fislandora%2Fobject%2Fislandora%3A33760%2Fdatastream%2FJP2%2Fview&svc_id=info%3Alanl-repo%2Fsvc%2FgetRegion&svc_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajpeg2000&svc.level=1
+            url = "https://digital.lafayette.edu/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id="+urllib.parse.quote(d("meta[property='og:image']").attr('content'), safe='').replace("https","http").replace("JPG","JP2")+"&svc_id=info%3Alanl-repo%2Fsvc%2FgetRegion&svc_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajpeg2000&svc.level=1"
+            # <meta name="DC.identifier" content="islandora:18707" />
+            filename = d("meta[name='DC.identifier']").attr('content').split(",")[0].replace(":","")+".jpg"
+            try:
+                download(url,filename,outputdir_path)
+                df.loc[i, 'thumbnail'] = filename
+            except:
+                df.to_csv(csv, encoding="utf-8", index=False)
+                print(url)
+                print(filename)
+                raise ConnectionError("error on : "+v["object_url"]+" (jpg)")
+            time.sleep(1)
+    df.to_csv(csv, encoding="utf-8", index=False)
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    main(args['<input_csv>'])
+    main(args['<csv>'])
